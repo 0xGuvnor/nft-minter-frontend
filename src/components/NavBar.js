21ef160251affdd318f64819b9bba50941f35511
0xbagholder <101540063+0xbagholder@users.noreply.github.com>
2022-06-19 19:32:35 +0800

    css adjustments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,49 +18,54 @@ const NavBar = ({ accounts, setAccounts }) => {
 
   return (
     <Flex justify="space-between" align="center" padding="30px">
-      <div>
-        {/* left side */}
-        <Flex justify="space-around" width="40%" padding="0 75px">
-          <Link href="https://www.facebook.com">
-            <Image src={Facebook} boxSize="42px" margin="0 15px" />
-          </Link>
-          <Link href="https://www.twitter.com">
-            <Image src={Twitter} boxSize="42px" margin="0 15px" />
-          </Link>
-          <Link href="https://www.gmail.com">
-            <Image src={Email} boxSize="42px" margin="0 15px" />
-          </Link>
-        </Flex>
+      {/* left side */}
+      <Flex justify="space-around" width="40%" padding="0 75px">
+        <Link href="https://www.facebook.com">
+          <Image src={Facebook} boxSize="42px" margin="0 15px" />
+        </Link>
+        <Link href="https://www.twitter.com">
+          <Image src={Twitter} boxSize="42px" margin="0 15px" />
+        </Link>
+        <Link href="https://www.gmail.com">
+          <Image src={Email} boxSize="42px" margin="0 15px" />
+        </Link>
+      </Flex>
 
-        {/* right side */}
-        <Flex justify="space-around" align="center" width="40%" padding="30px">
-          <Box margin="0 15px">About</Box>
-          <Spacer />
-          <Box margin="0 15px">Mint</Box>
-          <Spacer />
-          <Box margin="0 15px">Team</Box>
-          <Spacer />
+      {/* right side */}
+      <Flex justify="space-around" align="center" width="40%" padding="30px">
+        <Box margin="0 15px">About</Box>
+        <Spacer />
+        <Box margin="0 15px">Mint</Box>
+        <Spacer />
+        <Box margin="0 15px">Team</Box>
+        <Spacer />
 
-          {/* connect button */}
-          {isConnected ? (
-            <Box margin="0 15px">Connected</Box>
-          ) : (
-            <Button
-              backgroundColor="#D6517D"
-              borderRadius="5px"
-              boxShadow="0px 2px 2px 1px #0F0F0F"
-              color="white"
-              cursor="pointer"
-              fontFamily="inherit"
-              padding="15px"
-              margin="0 15px"
-              onClick={connectAccount}
-            >
-              Connect
-            </Button>
-          )}
-        </Flex>
-      </div>
+        {/* connect button */}
+        {isConnected ? (
+          <Box
+            backgroundColor="#D6517D"
+            borderRadius="5px"
+            boxShadow="0px 2px 2px 1px #0F0F0F"
+            color="white"
+            fontFamily="inherit"
+            padding="5px"
+          >{`${accounts[0].slice(0, 6)}...${accounts[0].slice(-4)}`}</Box>
+        ) : (
+          <Button
+            backgroundColor="#D6517D"
+            borderRadius="5px"
+            boxShadow="0px 2px 2px 1px #0F0F0F"
+            color="white"
+            cursor="pointer"
+            fontFamily="inherit"
+            padding="15px"
+            margin="0 15px"
+            onClick={connectAccount}
+          >
+            Connect
+          </Button>
+        )}
+      </Flex>
     </Flex>
   );
 };
